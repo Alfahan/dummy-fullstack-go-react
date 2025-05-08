3f376ddfd7fd509f9197ae5a0004e17a0230b729
Alfahan <ali.farhan160@gmail.com>
2025-05-08 18:55:22 +0700

    first-commit

---
+++ backend-api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"dummy-fullstack-go-react/backend-api/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	// Load config .env
+	config.LoadEnv()
+
+	//inisialiasai Gin
+	router := gin.Default()
+
+	//membuat route dengan method GET
+	router.GET("/", func(c *gin.Context) {
+
+		//return response JSON
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	//mulai server
+	router.Run(":" + config.GetEnv("APP_PORT", "3000"))
+
+}
