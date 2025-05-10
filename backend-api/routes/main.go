8de9465b7e017115afab1bd8929ed174289d3579
Alfahan <ali.farhan160@gmail.com>
2025-05-10 13:35:09 +0700

    feat: config-CORS

--- backend-api/routes/main.go
+++ backend-api/routes/main.go
@@ -5,10 +5,18 @@ import (
 	"dummy-fullstack-go-react/backend-api/middlewares"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func SetupRoutes() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 
 	router.POST("/api/register", controllers.Register)
 	router.POST("/api/login", controllers.Login)
